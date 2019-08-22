1663cf13ec94bdb4db3744b1c4ce683230ad4a29
abusamrah <abusamrahtechcampus@gmail.com>
2019-08-23 01:55:20 +0300

    edit 3.py

---
+++ 3.py
@@ -0,0 +1,23 @@
+
+"""
+التعامل مع النصوص والارقام
+"""
+
+# x and y  is string
+x = "Mohammed"
+y = "Asmri"
+
+c = x + " " + y
+
+# d and n  is int
+d = 6
+n = 6
+
+
+s = d * n
+a = d + n
+z = d - n
+print(a)
+print(s)
+print(z)
+print(c)
