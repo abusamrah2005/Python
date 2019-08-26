196c486e489e352c7c71e0a3ea63c4ad6a2e8cdf
abusamrah2005 <abusamrah2005@hotmail.com>
2019-08-26 22:38:53 +0300

    add day-7.py

---
+++ Week 2/Day-7.py
@@ -0,0 +1,44 @@
+"""
+السلاسل النصية / النصوص في لغة البايثون
+"""
+
+
+print("السلاسل النصية / النصوص في لغة البايثون")
+print('Hello World')  # ' '
+
+
+
+print("Hello World")   # " "
+
+print("--------------------------")
+
+# تعريف متغير يخزّن فيه نص 
+print("تعريف متغير يخزّن فيه نص ")
+
+text = "Welcom To Python Lessons"
+
+print(text)
+
+print("--------------------------")
+
+# تعريف متغير يخزّن فيه نص متعدد الاسطر
+print(" تعريف متغير يخزّن فيه نص متعدد الاسطر")
+install = """
+Python source code and installers
+are available for download for all
+versions!
+"""
+print(install)
+
+print("--------------------------")
+
+# السلاسل النصية هي مصفوفات
+
+# P y t h o n , Week 2, lesson
+# 1 2 3 4 5 6 
+
+MyText = "Python, Week 2, lesson"
+print(MyText) # All
+print(MyText[0:6])   # From 0 To 6 Python Word 
+print(MyText[7:14])  # From 7 To 14 Week 2 Word
+print(MyText[15:22]) # From 15 To 22 lesson Word
