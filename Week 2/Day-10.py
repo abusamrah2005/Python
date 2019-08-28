8a4e892340f94da4f1854beeae5925db93d78a6b
abusamrah2005 <abusamrah2005@hotmail.com>
2019-08-28 22:27:45 +0300

    Add Day-10.py

---
+++ Week 2/Day-10.py
@@ -0,0 +1,33 @@
+# العوامل في لغة البايثون
+# Python Operators
+
+print('Python Operators - العوامل في لغة البايثون')
+x = 6
+y = 4
+print ("x = 6 And y = 4")
+print("  6 * 4  result = " ,+ x * y)
+print("  6 + 4  result = " ,+ x + y)
+print("  6 - 4  result = " ,+ x - y)
+print("  6 / 4  result = " ,+ x / y)
+
+print("-------------------------------")
+
+# عوامل إعطاء قيم للمتغيرات/ التخصيص
+# Python Assignment Operators
+
+print(" Python Assignment Operators - عوامل إعطاء قيم للمتغيرات/ التخصيص")
+b = 6
+b += 2
+print("b = 6 and b += 2 ")
+print ("  result = ", b)
+print("-------------------------------")
+b = 6
+b *= 2
+print("b = 6 and b *= 2 ")
+print ("  result = ", b)
+
+print("-------------------------------")
+b = 6
+b -= 2
+print("b = 6 and b -= 2 ")
+print ("  result = ", b)
