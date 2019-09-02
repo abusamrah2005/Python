256bb685b847e64f18dc609ac5771d229ad03329
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-09-02 20:11:18 +0300

    Add Day-14.py

---
+++ Week 3/Day-14.py
@@ -0,0 +1,16 @@
+"""
+
+القوائم في لغة البايثون
+Python Lists 2
+"""
+# Data Structures - هياكل البيانات
+
+Devices = ["Apple", "Samsung", "Huawei", "Nokia"]
+
+# لكي نقوم بطباعة جزء من عناصر هذه القائمة نضع رقم الاندكس للعناصر
+
+print(Devices[0:])
+print(Devices[0:3])
+print(Devices[2:])
+print(Devices[3
+])
