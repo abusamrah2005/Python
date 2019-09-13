f70c9a51301e70292ac3cf80c6c8df2a614ea7b1
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-09-13 22:22:13 +0300

    Add file Week-4-Day-25-and-Day26-assignment.py

---
+++ Week-4/Week-4-Day-25-and-Day26-assignment.py
@@ -0,0 +1,22 @@
+# python Week4 Day25-26 . Challenge of the week4
+
+set1 = {1,3,5,7,8}
+print(set1)
+# Add multiple items to my set.
+set1.update([4,8,12])
+print("Add [4,8,12] To My Set : ", set1)
+# remove an item in my set.
+set1.remove(8)
+print("Delete number 8 From My Set : ", set1)
+
+print("------")
+
+my_dict = {"type":"bird", "skin cover":"feathers"}
+print(my_dict)
+type = my_dict["type"]
+print(type)
+print("-------")
+my_dict["skin cover"] = "Scales"
+print(my_dict)
+print("-------")
+
