8fb17843ae1ff5db11f052deeef5eedcea4f26a9
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-09-13 22:58:36 +0300

    Add file training.py

---
+++ Week-4/training.py
@@ -0,0 +1,24 @@
+# python Week4 Day25-26 . Challenge of the week4
+
+set1 = {1,3,5,7,8}
+print(set1)
+# Add multiple items to my set.
+set1.add (int(input("Add number Only : ")))
+print(set1)
+# remove an item in my set.
+
+set1.remove (int(input("Delete number : ")))
+print(set1)
+
+print("------")
+
+my_dict = {"type":"bird", "skin cover":"feathers"}
+print(my_dict)
+type = my_dict["type"]
+my_dict.add["eee":"eee"]
+print(type)
+print("-------")
+my_dict["skin cover"] = "Scales"
+print(my_dict)
+print("-------")
+
