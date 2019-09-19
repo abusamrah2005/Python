b0dfe84f9897d6f9f73fc83a9bf98f5f6aac451d
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-09-20 01:15:50 +0300

    Add Day-31.py

--- Week-5/Day-31.py
+++ Week-5/Day-31.py
@@ -1,33 +1,23 @@
 # Python Week-5 Day-31
-
 # Python Functions
-
 # Creating a Function -- In Python a function is defined using the def keyword
-
 
 def my_function():
     print(" This Is My First Function ")
 
 # To call a function, use the function name followed by parenthesis
-
-
 my_function()
-
 # Parameters
 print("----")
-
-
 def country(name, age, country1):
     print('My Name Is: ', name)
     print('I am ', age, 'old')
     print("I am From ", country1)
 
-
 while True:
     name = input("Enter Your Name: ")
     age = int(input("Enter Your Age : "))
     country1 = input(" Where Are You From: ")
-
 
     country(name, age, country1)
   
