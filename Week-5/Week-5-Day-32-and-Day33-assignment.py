ca5b5a27d7af0f460ed7e31a261c1522619ba63d
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-09-20 17:06:55 +0300

    Edit Week-5-Day-32-and-Day33-assignment.py

--- Week-5/Week-5-Day-32-and-Day33-assignment.py
+++ Week-5/Week-5-Day-32-and-Day33-assignment.py
@@ -2,8 +2,8 @@
 A = (3,5,7,9,11,13,15,17,)
 B = (2,4,6,8,10,12,14,16)
 
-for T in A:
-    for Y in B:
+for T in range(3, 18, 2):
+    for Y in range(2, 17, 2 ):
         print (T,"=>", Y)
 
 input('Press ENTER to continue....')
