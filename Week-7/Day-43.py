ace6c86f4311914ec89d75d97a600d019bedfb6d
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-10-01 21:34:05 +0300

    Add file Day-43.py

---
+++ Week-7/Day-43.py
@@ -0,0 +1,20 @@
+# # Python Week-7 Day-43
+# Python Inheritance
+
+class Person: #------->  Super Class
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person): # ----> Sub Class
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname) 
+
+
+x = Person ("\nPerson Class:  "+" Mohammed","Al Asmri" + " Called From Super Class( Person)")
+x.printname()
+y = Student("\nStudent Class: "+" Mohammed","Al Asmri" + " Called From Sub Class( Student)")
+y.printname()
