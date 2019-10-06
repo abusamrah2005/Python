1bbdf3dbfa7675eaba39e67adaf0d6d2eb3ffba2
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-10-06 19:21:16 +0300

    Add file Day-48.py

--- Week-8/Day-48.py
+++ Week-8/Day-48.py
@@ -1,6 +1,5 @@
 # Python Week-7 Day-48
 # Python Scope
-
 # A variable created inside a function is available inside that function
 print("\n-----")
 def myfunc():
@@ -10,7 +9,6 @@ myfunc()
 print("\n-----")
 # The local variable can be accessed from 
 # a function within the function.
-
 def myfunc1():
     xx = "Call Me"
     def myfunc2():
@@ -20,14 +18,13 @@ myfunc1()
 print("\n-----")
 # cA variable created outside of a function is
 #  global and can be used by anyone
-
 y = "Global Variable"
 def my_global():
     global yy # global variable Inside function
     yy = "variable inside function "
     print( y, " => inside function")
-
 my_global() 
+
 print("\n-----")
 print(y, " => OutSide function")
 print("\n-----")
