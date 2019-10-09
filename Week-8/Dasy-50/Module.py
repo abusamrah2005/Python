a3a3497c4ce47e7b12672895d592b3e3db9716d7
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-10-09 17:15:45 +0300

    add Day-50 Folder

---
+++ Week-8/Dasy-50/Module.py
@@ -0,0 +1,16 @@
+import mymodule
+
+mymodule.greeting(" Mohammed Al Asmri")
+print("-------")
+a = mymodule.person1["age"]
+print(a)
+print("-------")
+print("-------")
+
+a = mymodule.person1["name"]
+b = mymodule.person1["age"]
+c = mymodule.person1["country"]
+print(" My Name Is ", a,  " Iam ", b,  "years Old",  " iam from ", c)
+
+
+mymodule.my_childs1(" Abduallah", " Danh", " Ahmed")
---
+++ Week-8/Dasy-50/mymodule.py
@@ -0,0 +1,15 @@
+def greeting(name):
+    print(" Hello ," + name)
+
+person1 = {
+  "name": "Mohammed",
+  "age": 38,
+  "country": "Saudia Arabia"
+}
+
+
+def my_childs1(*kids):
+    print(" The First child is " + kids[0])
+    print(" The Second child is " + kids[1])
+    print(" The Third child is " + kids[2])
+    print(" Allah protect them")
