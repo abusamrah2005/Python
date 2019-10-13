a65f0be8bdb1d3b3b8d7965df540ee08a5507f41
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-10-14 00:02:39 +0300

    Add Folder Week-9

---
+++ Week-9/Day-55.py
@@ -0,0 +1,45 @@
+# Python Week-9 Day-55
+# Python JSON
+
+import json
+
+print("\n---------")
+
+x =  '{ "name":"Mohammed", "age":38, "city":"Abha"}'
+
+y = json.loads(x)
+print(y["age"])
+print("---------")
+x = {
+  "name": "Mohammed Asmri",
+  "age": 38,
+  "city": "Yanbu"
+}
+y = json.dumps(x)
+print(y)
+print("---------")
+
+print(json.dumps({"name": "Ahmad", "age": 3}))
+print(json.dumps(["Toyota", "BMW"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("Welcome Home"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
+print("---------")
+x = {
+  "name": "Mohammed",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Abduallah","Danh", "Ahmad"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
