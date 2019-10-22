7b0862b38ad4633b0ebf94ef5e8550de3e63efee
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-10-22 22:59:03 +0300

    Add file Day-63.py

---
+++ Week-10/Day-63.py
@@ -0,0 +1,10 @@
+# Python Week-10 Day-63
+# Python Command Line Input
+
+string1 = input(" Type Any Thing: ")
+
+print(" The text you typed is", string1)
+print("-----")
+fname = input(" Enter Your First Name: ")
+lname = input(" Enter Your Last Name: ")
+print ("Welcome Back", fname + " " + lname)
