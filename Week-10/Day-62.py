89ccfbc5d433cd264e1280f3730f6286c1ad0f70
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-10-22 22:18:22 +0300

    add folder Week-10

---
+++ Week-10/Day-62.py
@@ -0,0 +1,11 @@
+# Python Week-10 Day-62
+# Python PIP
+
+
+import camelcase
+
+c = camelcase.CamelCase()
+txt = "\n\n python is an interpreted, high-level, general-purpose programming language"
+print(c.hump(txt))
+
+
