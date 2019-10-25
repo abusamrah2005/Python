dc289b89fed42cea93c5e32bdb0d3dfc0c5eb108
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-10-25 22:16:33 +0300

    Add File Week-10-Challenge.py

---
+++ Week-10/Week-10-Challenge.py
@@ -0,0 +1,12 @@
+# Week-10 Challe
+latter1 = input("Enter first Latter in Your Name: ")
+latter2 = input("Enter Last Latter in Your Name: ")
+print("First Latter is {}".format(latter1), "Scocend Latter is {}".format(latter2) )
+print("-----")
+print("-----")
+
+my_palance = "Dear {}, Your current balance is {}{}"
+name = "Mohammed Asmri"
+palance = 53.44
+currency = "$"
+print(my_palance.format(name, palance, currency))
