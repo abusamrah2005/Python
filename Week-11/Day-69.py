670e9b22273fd9b470822b315ab313df49d2ff60
“abusamrah2005” <abusamrah2005@hotmail.com>
2019-10-27 23:54:32 +0300

    Add Week-11 Folder

---
+++ Week-11/Day-69.py
@@ -0,0 +1,17 @@
+
+# File Handling in Python
+
+
+
+x = open("Week-11/myfile.txt", "r")
+print(x.read())
+x.close()
+
+
+# name = input("ُEnter Your Name : ")
+# age = input("ُEnter Your Age : ")
+
+# f = open("Week-11/Day-69.txt", "a")
+# f.write(" Your Name Is " + name + "\n"+ " Your Age is "+ age + "\n")
+# f.close()
+
